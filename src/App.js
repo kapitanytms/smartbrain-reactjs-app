bbb574fcb7f0e8bd7d3528e130e152ea8ff09295
Tamas Kapitany <kapitanytms@gmail.com>
2018-02-17 20:01:51 +0100

    face detect works well with one face

--- src/App.js
+++ src/App.js
@@ -29,24 +29,37 @@ class App extends Component {
     super();
     this.state = {
       input: '',
+      box: {}
+    }
+  }
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
     }
   }
 
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+  }
   onInputChange = (event) => {
     this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () => {
     console.log('Click!');
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-    function(response) {
-      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-    },
-    function(err) {
-      // there was an error
-    }
-  );
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err));
   }
+
   render() {
     return (
       <div className="App">
@@ -57,7 +70,7 @@ class App extends Component {
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-        <FaceRecognition input={this.state.input} />
+        <FaceRecognition box={this.state.box} input={this.state.input} />
       </div>
 
     );
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './FaceRecognition.css';
 
-const FaceRecognition = ({input}) => {
+const FaceRecognition = ({input, box}) => {
     return (
         <div className="center ma">
             <div className="absolute mt2">
-                <img alt="" src={input} width="500px" height="auto"/>
+                <img id="inputimage" alt="" src={input} width="500px" height="auto"/>
+                <div className="bounding-box" style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
             </div>
         </div>
     );
